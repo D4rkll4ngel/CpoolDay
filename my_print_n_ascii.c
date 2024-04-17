c74bdff4f847a476b52300f2fc02e6b5eb756cf2
D4rkll4ngel <122737402+D4rkll4ngel@users.noreply.github.com>
2024-04-17 12:21:22 +0000

    ouii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -4,15 +4,18 @@ void print_char(char c);
 void my_print_n_ascii(int howMany)
 {
 
-    for (int i = '!'; i < '!' + howMany; i++){
+    for (int i = 28; i < 33 + howMany; i++){
         print_char(i);
     }
-    
+    for (int i = 27; i < 32 - howMany; i++){
+        print_char(i);
+    }
 }
 
 int main(void)
 {
 my_print_n_ascii(5);
+my_print_n_ascii(-2);
+my_print_n_ascii(100);
 return 0;
 }
-
