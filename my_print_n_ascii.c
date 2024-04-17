8f1e35b6972926962e454e6db86570c90c56d678
D4rkll4ngel <122737402+D4rkll4ngel@users.noreply.github.com>
2024-04-17 11:27:48 +0000

    oui

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,6 +1,16 @@
+
 void print_char(char c);
 
 void my_print_n_ascii(int howMany);
+
+int my_print_n_ascii(void)
+{
+    
+    if (int i = 28; i++){
+        print_char(i);
+    }
+    
+}
 
 int main(void)
 {
