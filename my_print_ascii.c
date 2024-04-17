c56c498002023502abed86d8ba441ddb0c7dc93e
D4rkll4ngel <122737402+D4rkll4ngel@users.noreply.github.com>
2024-04-17 12:55:00 +0000

    azertyu

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -3,9 +3,8 @@ void print_char(char c);
 int my_print_ascii(void)
 {
     
-    for (int i = 28; i <= 127; i++){
+    for (int i = 33; i <= 126; i++){
         print_char(i);
     }
-    
-    
 }
+
