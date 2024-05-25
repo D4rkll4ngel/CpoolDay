895a007997d974e7ceac27974709bb6810ff06b8
D4rkll4ngel <122737402+D4rkll4ngel@users.noreply.github.com>
2024-05-25 13:39:19 +0000

    oui

--- my_char_replace.c
+++ my_char_replace.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-char *my_char_replace(char *origin, char toFind, char toReplace);
-
-for (int i = 0; origin[i])
-
-int main(int ac, char **av)
-{
-    char str[] = "bienvenue à epitech !";
-    printf("%s\n", my_char_replace(str, 'e', 'a'));
-    return 0;
+char *my_char_replace(char *origin, char toFind, char toReplace) {
+    for (int i =0; origin[i] != '\0'; i++) {
+        if (origin[i] == toFind) {
+            origin[i] = toReplace;
+        }
+    }
+    return origin;
 }
