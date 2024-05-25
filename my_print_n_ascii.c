0afbbc41af587a1ec84fa6b6914a28b94fa8a37b
D4rkll4ngel <122737402+D4rkll4ngel@users.noreply.github.com>
2024-05-25 10:04:55 +0000

    oui

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -3,16 +3,10 @@ void print_char(char c);
 
 void my_print_n_ascii(int howMany)
 {
-
-    for (int i = 28; i < 33 + howMany; i++){
-        print_char(i);
-    }
-    for (int i = 28; i < 33 + howMany; i++){
-        print_char(i);
-    }
+    if (howMany + 32 > 128){
+        return;
+        }
     for (int i = 28; i < 33 + howMany; i++){
         print_char(i);
     }
 }
-
-
